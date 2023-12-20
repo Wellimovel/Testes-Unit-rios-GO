40dc33503bad6c6e49f76ec984e7fc73940ef602
Wellington <wellington@wellimovel.com>
2023-12-20 09:39:14 -0300

    first commit

---
+++ soma.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := soma(1, 5, 3)
+	fmt.Println(x)
+
+}
+
+func soma(i ...int) int {
+	total := 0
+	for _, v := range i {
+		total += v
+	}
+	return total
+}
